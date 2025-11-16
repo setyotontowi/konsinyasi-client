e5d8a3386106b33a373c35550404ad940d31439c
setyotontowi <setyotontowi@gmail.com>
2025-11-16 19:01:28 +0700

    Top bar username

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { jwtDecode } from "jwt-decode";
 
 export default function Topbar() {
+
+  let username = "";
+  let unit = ""
+
+  try {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      console.log(decoded);
+      username = decoded.name || "";
+      unit = decoded.unit_name ? ` - ${decoded.unit_name}` : "";
+    }
+  } catch (err) {
+    console.error("JWT decode failed:", err);
+  }
+
+
   return (
     <div className="flex items-center justify-between bg-white border-b border-b-gray-200 p-4">
-      <h1 className="text-xl font-semibold">Welcome, Admin</h1>
-      <div className="text-sm text-gray-600">Admin panel</div>
+      <h1 className="text-xl font-semibold"></h1>
+      <div className="text-sm text-gray-600">{username ? username : "User"}{unit}</div>
     </div>
   )
 }
